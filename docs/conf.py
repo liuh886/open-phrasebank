62b9ad8ebccd907f2d39681fe10aec4b54d9c3ca
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2024-04-14 20:39:11 +0000

    update sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,28 @@ release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- General configuration ---------------------------------------------------
+#
 
-extensions = ['nbsphinx']
+extensions = [
+    # Sphinx's own extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # Our custom extension, only meant for Furo's own documentation.
+    "furo.sphinxext",
+    # External stuff
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
+    # notebook
+    'nbsphinx',
+]
+
 nbsphinx_execute = 'never'
 html_logo = "_static/opb_logo.png"
 pygments_style = 'sphinx'
@@ -23,8 +43,25 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 source_suffix = ['.rst', '.md', '.ipynb']  # Add '.ipynb'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Options for TODOs -------------------------------------------------------
+#
 
-html_theme = 'furo'
-html_static_path = ['_static']
+todo_include_todos = True
+
+# -- Options for Markdown files ----------------------------------------------
+#
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
+
+# -- Options for HTML output -------------------------------------------------
+#
+
+html_theme = "furo"
+html_title = "Furo"
+language = "en"
+
+html_static_path = ["_static"]
