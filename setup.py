43694d464bce2ef69b6d342e6dd0322cc884cc39
Zhihao LIU <7567311+liuh886@users.noreply.github.com>
2024-04-15 20:51:01 +0000

    pytest

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.8',
     entry_points={
         'console_scripts': [
             'openphrasebank=openphrasebank.cli:main',  # Adjust according to your package structure
